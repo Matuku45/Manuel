bd1f496c9429179be9fa81a4b1e5450baad42119
Thabiso <thabiso@example.com>
2024-10-20 01:50:02 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, Welcome to My React App!</h1>
+      <p>Start coding here.</p>
+    </div>
+  );
+}
+
+export default App;
